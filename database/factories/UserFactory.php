a701d80589ed543d8bd7962165e0cce51a8fc7a5
ashishsehrawat04 <ashishkumarjjr@gmail.com>
2025-08-19 16:18:09 +0530

    second

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,7 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
-/**
+/** //  this is second change
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
 class UserFactory extends Factory
